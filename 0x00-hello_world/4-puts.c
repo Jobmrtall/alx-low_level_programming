3261bd492f111aeeca6f93c2ad4a6b3a0c344b1e
Jobmrtall <jobkelly0@gmail.com>
2023-02-16 17:18:37 -0800

    last but not least

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *
  * Description: 'master'
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  *  main - Entry Point
  *
- *  Return : Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 
 
@@ -11,6 +11,7 @@ int main(void)
 	printf("size of char: %lu byte(s)\n", sizeof(char));
 	printf("size of int: %lu byte(s)\n", sizeof(int));
 	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 
