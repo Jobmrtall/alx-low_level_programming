876f58ba7b7ebf152c2e3538bd8996bedb1f0d3f
Jobmrtall <jobkelly0@gmail.com>
2023-02-20 16:30:36 -0800

    .....

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *description: lowercase alphabets
+ *Return: always 0
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
