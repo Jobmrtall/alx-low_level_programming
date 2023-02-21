aa879eea4ae6bee9f42f6622825bb53d2bc00b16
Jobmrtall <jobkelly0@gmail.com>
2023-02-20 19:19:48 -0800

    16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: 'print both upper and lowercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int k;
+
+	for (n == 48; k <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (k == 97; k <= 102; k++)
+	{
+		putchar(k);
+	}
+	putchar ('\n');
+	return (0);
+}
